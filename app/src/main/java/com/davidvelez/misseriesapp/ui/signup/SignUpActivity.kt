1fef65eff4a7f0edb55251db9b4e9c3a262d5ebf
DavidFVelez <davidf.velez@udea.edu.co>
2023-03-22 11:05:44 -0500

    Actualizaciones LiveData

--- app/src/main/java/com/davidvelez/misseriesapp/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/davidvelez/misseriesapp/ui/signup/SignUpActivity.kt
@@ -6,14 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import com.davidvelez.misseriesapp.databinding.ActivitySignUpBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var signUpBinding: ActivitySignUpBinding
-    private var fechaNacimiento: String = ""
-    private var cal = Calendar.getInstance()
+    private lateinit var signUpViewModel: SignUpViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,111 +23,215 @@ class SignUpActivity : AppCompatActivity() {
         val view = signUpBinding.root
         setContentView(view)
 
-        val dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, month)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        signUpViewModel = ViewModelProvider(this)[SignUpViewModel::class.java]
 
-                val format = "MM/dd/yyyy"
-                val sdf = SimpleDateFormat(format, Locale.US)
-                fechaNacimiento = sdf.format(cal.time).toString()
-                signUpBinding.fechaNacimientoEditText.setText(fechaNacimiento)
-            }
-        signUpBinding.fechaNacimientoEditText.setOnClickListener {
-            DatePickerDialog(
-                this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
-            ).show()
 
+        val errorEmailObserver = Observer<String> { errorEmailOut ->
+            signUpBinding.emailEditText.error =
+                "Error. El email debe contener @"
         }
+        signUpViewModel.errorEmailOut.observe(this, errorEmailObserver)
+
+
+        val infoOutObserver = Observer<String> { infoOut ->
+            signUpBinding.infoTextView.setText(infoOut)
+        }
+        signUpViewModel.infoOut.observe(this, infoOutObserver)
+
+        val errorPasswordObserver = Observer<String> { errorPasswordOut ->
+            Toast.makeText(
+                applicationContext,
+                "Las contraseñas no son iguales",
+                Toast.LENGTH_SHORT
+            ).show()
+            signUpBinding.repPasswordEditText.setText("")
+        }
+        signUpViewModel.errorPasswordOut.observe(this, errorPasswordObserver)
+
+        val errorEmptyNameOutObserver = Observer<String> { errorEmptyNameOut ->
+            signUpBinding.nameEditText.error =
+                "Este campo es obligatorio"
+        }
+        signUpViewModel.errorEmptyNameOut.observe(this, errorEmptyNameOutObserver)
+
+        val errorEmptyEmailOutObserver = Observer<String> { errorEmptyEmailOut ->
+            signUpBinding.emailEditText.error =
+                "Este campo es obligatorio"
+        }
+        signUpViewModel.errorEmptyEmailOut.observe(this, errorEmptyEmailOutObserver)
+
+
+        val errorEmptyPasswordOutObserver = Observer<String> { errorEmptyPasswordOut ->
+            signUpBinding.passwordEditText.error =
+                "Este campo es obligatorio"
+        }
+        signUpViewModel.errorEmptyPasswordOut.observe(this, errorEmptyPasswordOutObserver)
+
+        val errorEmptyRepPasswordOutObserver = Observer<String> { errorEmptyRepPasswordOut ->
+            signUpBinding.repPasswordEditText.error =
+                "Este campo es obligatorio"
+        }
+        signUpViewModel.errorEmptyRepPasswordOut.observe(this, errorEmptyRepPasswordOutObserver)
+
+        val errorEmptyBirthDateOutObserver = Observer<String> { errorEmptyBirthDateOut ->
+            Toast.makeText(
+                applicationContext,
+                "La fecha de nacimiento es obligatoria",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        signUpViewModel.errorEmptyBirthDateOut.observe(this, errorEmptyBirthDateOutObserver)
+
+        val errorplaceOfBirthOutObserver = Observer<String> { errorplaceOfBirthOut ->
+
+            Toast.makeText(
+                applicationContext,
+                "Por favor seleccionar el lugar de nacimiento",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        signUpViewModel.errorplaceOfBirthOut.observe(this, errorplaceOfBirthOutObserver)
+
+
+//        val dateSetListenerObserver =Observer<String> { dateSetListenerOut ->
+//
+//        }
+//
+//        signUpViewModel.dateSetListenerOut.observe(this, dateSetListenerObserver)
+//
+//        signUpBinding.fechaNacimientoEditText.setOnClickListener {
+//            val calObserver = Observer<String> { calOut ->
+//                DatePickerDialog(
+//                    this, dateSetListener, calOut.get(Calendar.YEAR), calOut.get(Calendar.MONTH),
+//                    calOut.get(Calendar.DAY_OF_MONTH)
+//                ).show()
+//            }
+//            signUpViewModel.calOut.observe(this, calObserver)
+//
+//
+//
+//            costCalculatorViewModel.fixedCost.observe(this, fixedCostObserver)
+//
+//            signUpViewModel.spinnerDate()
+//            val dateSetListener =
+//                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+//                    cal.set(Calendar.YEAR, year)
+//                    cal.set(Calendar.MONTH, month)
+//                    cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+//
+//                    val format = "MM/dd/yyyy"
+//                    val sdf = SimpleDateFormat(format, Locale.US)
+//                    fechaNacimiento = sdf.format(cal.time).toString()
+//                    signUpBinding.fechaNacimientoEditText.setText(fechaNacimiento)
+//                }
+//            signUpBinding.fechaNacimientoEditText.setOnClickListener {
+//                DatePickerDialog(
+//                    this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+//                    cal.get(Calendar.DAY_OF_MONTH)
+//                ).show()
+//
+//            }
 
         signUpBinding.registerButton.setOnClickListener {
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(signUpBinding.fechaNacimientoTextInputLayout.windowToken, 0)
 
-            val name = signUpBinding.nameEditText.text.toString()
-            val email = signUpBinding.emailEditText.text.toString()
-            val password = signUpBinding.passwordEditText.text.toString()
-            val repPassword = signUpBinding.repPasswordEditText.text.toString()
-            val genre = if (signUpBinding.maleRadioButton.isChecked)
-                "Masculino"
-            else
-                "Femenino"
+            signUpViewModel.registerUser(
+                signUpBinding.nameEditText.text.toString(),
+                signUpBinding.emailEditText.text.toString(),
+                signUpBinding.passwordEditText.text.toString(),
+                signUpBinding.repPasswordEditText.text.toString(),
+                signUpBinding.maleRadioButton.isChecked,
+                signUpBinding.actionCheckBox.isChecked,
+                signUpBinding.adventureCheckBox.isChecked,
+                signUpBinding.comicCheckBox.isChecked,
+                signUpBinding.loveCheckBox.isChecked,
+                signUpBinding.suspenseCheckBox.isChecked,
+                signUpBinding.horrorCheckBox.isChecked,
+                signUpBinding.fechaNacimientoEditText.text.toString(),
+                signUpBinding.spCiudad.selectedItem.toString()
+            )
 
-            var favoritesGenre = ""
-            if (signUpBinding.actionCheckBox.isChecked) favoritesGenre = "Acción"
-            if (signUpBinding.adventureCheckBox.isChecked) favoritesGenre += " Aventura"
-            if (signUpBinding.comicCheckBox.isChecked) favoritesGenre += " Humor"
-            if (signUpBinding.loveCheckBox.isChecked) favoritesGenre += " Amor"
-            if (signUpBinding.suspenseCheckBox.isChecked) favoritesGenre += " Suspenso"
-            if (signUpBinding.horrorCheckBox.isChecked) favoritesGenre += " Terror"
+//            val name = signUpBinding.nameEditText.text.toString()
+//            val email = signUpBinding.emailEditText.text.toString()
+//            val password = signUpBinding.passwordEditText.text.toString()
+//            val repPassword = signUpBinding.repPasswordEditText.text.toString()
+//            val genre = if (signUpBinding.maleRadioButton.isChecked)
+//                "Masculino"
+//            else
+//                "Femenino"
+//
+//            var favoritesGenre = ""
+//            if (signUpBinding.actionCheckBox.isChecked) favoritesGenre = "Acción"
+//            if (signUpBinding.adventureCheckBox.isChecked) favoritesGenre += " Aventura"
+//            if (signUpBinding.comicCheckBox.isChecked) favoritesGenre += " Humor"
+//            if (signUpBinding.loveCheckBox.isChecked) favoritesGenre += " Amor"
+//            if (signUpBinding.suspenseCheckBox.isChecked) favoritesGenre += " Suspenso"
+//            if (signUpBinding.horrorCheckBox.isChecked) favoritesGenre += " Terror"
+//
+//            val birthDate = signUpBinding.fechaNacimientoEditText.text.toString()
+//            val placeOfBirth = signUpBinding.spCiudad.selectedItem.toString()
 
-            val birthDate = signUpBinding.fechaNacimientoEditText.text.toString()
-            val placeOfBirth = signUpBinding.spCiudad.selectedItem.toString()
-
-            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && repPassword.isNotEmpty() && birthDate.isNotEmpty() && placeOfBirth.isNotEmpty() &&placeOfBirth != "Seleccionar") {
-                if (password == repPassword && password.isNotEmpty()) {
-
-                    if (email.indexOf("@") != -1) {
-                        val info =
-                            "Nombre: $name\nEmail: $email\nPassword: $password\nGenre: $genre\nFavorites Genre: $favoritesGenre\nBirth Date: $birthDate\nPlace Of Birth: $placeOfBirth"
-                        signUpBinding.infoTextView.setText(info.toString())
-
-                    } else {
-                        signUpBinding.emailEditText.error =
-                            "Error. El email debe contener @"
-                    }
-
-                } else {
-                    Toast.makeText(
-                        applicationContext,
-                        "Las contraseñas no son iguales",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    signUpBinding.repPasswordEditText.setText("")
-                }
-            } else {
-
-                val arrayFields =
-                    arrayOf(name, email, password, repPassword, birthDate)
-
-                if (arrayFields[0].isEmpty()) {
-                    signUpBinding.nameEditText.error =
-                        "Este campo es obligatorio"
-                }
-                if (arrayFields[1].isEmpty()) {
-                    signUpBinding.emailEditText.error =
-                        "Este campo es obligatorio"
-                }
-                if (arrayFields[2].isEmpty()) {
-                    signUpBinding.passwordEditText.error =
-                        "Este campo es obligatorio"
-                }
-                if (arrayFields[3].isEmpty()) {
-                    signUpBinding.repPasswordEditText.error =
-                        "Este campo es obligatorio"
-
-                }
-
-                if (arrayFields[4].isEmpty()) {
-                    Toast.makeText(
-                        applicationContext,
-                        "La fecha de nacimiento es obligatoria",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                if (placeOfBirth == "Seleccionar") {
-
-                    Toast.makeText(
-                        applicationContext,
-                        "Por favor seleccionar el lugar de nacimiento",
-                        Toast.LENGTH_SHORT
-                    ).show()
-
-                }
-
-            }
+//            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && repPassword.isNotEmpty() && birthDate.isNotEmpty() && placeOfBirth.isNotEmpty() && placeOfBirth != "Seleccionar") {
+//                if (password == repPassword && password.isNotEmpty()) {
+//
+//                    if (email.indexOf("@") != -1) {
+//                        val info =
+//                            "Nombre: $name\nEmail: $email\nPassword: $password\nGenre: $genre\nFavorites Genre: $favoritesGenre\nBirth Date: $birthDate\nPlace Of Birth: $placeOfBirth"
+//                        signUpBinding.infoTextView.setText(info.toString())
+//
+//                    } else {
+//                        signUpBinding.emailEditText.error =
+//                            "Error. El email debe contener @"
+//                    }
+//
+//                } else {
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "Las contraseñas no son iguales",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                    signUpBinding.repPasswordEditText.setText("")
+//                }
+//            } else {
+//
+//                val arrayFields =
+//                    arrayOf(name, email, password, repPassword, birthDate)
+//
+//                if (arrayFields[0].isEmpty()) {
+//                    signUpBinding.nameEditText.error =
+//                        "Este campo es obligatorio"
+//                }
+//                if (arrayFields[1].isEmpty()) {
+//                    signUpBinding.emailEditText.error =
+//                        "Este campo es obligatorio"
+//                }
+//                if (arrayFields[2].isEmpty()) {
+//                    signUpBinding.passwordEditText.error =
+//                        "Este campo es obligatorio"
+//                }
+//                if (arrayFields[3].isEmpty()) {
+//                    signUpBinding.repPasswordEditText.error =
+//                        "Este campo es obligatorio"
+//
+//                }
+//
+//                if (arrayFields[4].isEmpty()) {
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "La fecha de nacimiento es obligatoria",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//                if (placeOfBirth == "Seleccionar") {
+//
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "Por favor seleccionar el lugar de nacimiento",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//
+//                }
+//
+//            }
 
         }
 
--- app/src/main/java/com/davidvelez/misseriesapp/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/davidvelez/misseriesapp/ui/signup/SignUpViewModel.kt
@@ -1,9 +1,185 @@
 package com.davidvelez.misseriesapp.ui.signup
 
-class SignUpViewModel {
+import android.app.DatePickerDialog
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SignUpViewModel : ViewModel() {
+
+    val calOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val infoOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmailOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorPasswordOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmptyNameOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmptyEmailOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmptyPasswordOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmptyRepPasswordOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorEmptyBirthDateOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val errorplaceOfBirthOut: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 
 
+    fun registerUser(
+        name: String,
+        email: String,
+        password: String,
+        repPassword: String,
+        maleRadioButton: Boolean,
+        actionCheckBox: Boolean,
+        adventureCheckBox: Boolean,
+        comicCheckBox: Boolean,
+        loveCheckBox: Boolean,
+        suspenseCheckBox: Boolean,
+        horrorCheckBox: Boolean,
+        birthDate: String,
+        placeOfBirth: String
+    ) {
 
+        val genre = if (maleRadioButton)
+            "Masculino"
+        else
+            "Femenino"
+
+        var favoritesGenre = ""
+        if (actionCheckBox) favoritesGenre = "Acción"
+        if (adventureCheckBox) favoritesGenre += " Aventura"
+        if (comicCheckBox) favoritesGenre += " Humor"
+        if (loveCheckBox) favoritesGenre += " Amor"
+        if (suspenseCheckBox) favoritesGenre += " Suspenso"
+        if (horrorCheckBox) favoritesGenre += " Terror"
+
+        checkFields(
+            name,
+            email,
+            password,
+            repPassword,
+            birthDate,
+            placeOfBirth,
+            genre,
+            favoritesGenre
+        )
+
+    }
+
+    private fun checkFields(
+        name: String,
+        email: String,
+        password: String,
+        repPassword: String,
+        birthDate: String,
+        placeOfBirth: String,
+        genre: String,
+        favoritesGenre: String
+    ) {
+
+        if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && repPassword.isNotEmpty() && birthDate.isNotEmpty() && placeOfBirth.isNotEmpty() && placeOfBirth != "Seleccionar") {
+            if (password == repPassword && password.isNotEmpty()) {
+
+                if (email.indexOf("@") != -1) {
+                    infoOut.value =
+                        "Nombre: $name\nEmail: $email\nPassword: $password\nGenre: $genre\nFavorites Genre: $favoritesGenre\nBirth Date: $birthDate\nPlace Of Birth: $placeOfBirth"
+//                    signUpBinding.infoTextView.setText(info)
+
+                } else {
+                    errorEmailOut.value = "on"
+
+                }
+
+            } else {
+
+                errorPasswordOut.value = "on"
+            }
+        } else {
+
+            val arrayFields =
+                arrayOf(name, email, password, repPassword, birthDate)
+
+            if (arrayFields[0].isEmpty()) {
+                errorEmptyNameOut.value = "on"
+
+
+            }
+
+            if (arrayFields[1].isEmpty()) {
+
+                errorEmptyEmailOut.value = "on"
+
+            }
+            if (arrayFields[2].isEmpty()) {
+                errorEmptyPasswordOut.value = "on"
+
+            }
+            if (arrayFields[3].isEmpty()) {
+                errorEmptyRepPasswordOut.value = "on"
+
+            }
+
+            if (arrayFields[4].isEmpty()) {
+
+                errorEmptyBirthDateOut.value = "on"
+
+            }
+            if (placeOfBirth == "Seleccionar" || placeOfBirth == "Select" || placeOfBirth =="Selezionare") {
+                errorplaceOfBirthOut.value = "on"
+            }
+
+        }
+
+
+    }
+
+// fun spinnerDate(){
+//     var fechaNacimiento: String = ""
+//     var cal = Calendar.getInstance()
+//
+//     val calOut: MutableLiveData<Int> by lazy {
+//         MutableLiveData<Int>()
+//     }
+//
+//     val dateSetListener =
+//         DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+//             cal.set(Calendar.YEAR, year)
+//             cal.set(Calendar.MONTH, month)
+//             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+//
+//             val format = "MM/dd/yyyy"
+//             val sdf = SimpleDateFormat(format, Locale.US)
+//             fechaNacimiento = sdf.format(cal.time).toString()
+//             signUpBinding.fechaNacimientoEditText.setText(fechaNacimiento)
+//         }
+//
+// }
 
 
 }
